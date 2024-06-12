44c9d814ac6d8082d679e2bbce96b4d99f7d4410
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-06-12 19:37:11 +0900

    Fix; ./

--- src/posts/post.module.ts
+++ src/posts/post.module.ts
@@ -1,14 +1,14 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Posts, PostsSchema } from './schema/posts.schema';
-import { UsersController } from 'src/users/users.controller';
-import { UsersService } from 'src/users/users.service';
+import { PostsService } from './posts.service';
+import { PostsController } from './post.controller';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Posts.name, schema: PostsSchema }]),
   ],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [PostsController],
+  providers: [PostsService],
 })
 export class PostsModule {}
